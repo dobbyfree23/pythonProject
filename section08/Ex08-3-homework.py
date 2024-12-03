e884c997f1684b374783548fcc6ec3518809b21b
ITSC <itsc@example.com>
2024-12-03 19:22:54 +0900

    section08

--- section08/Ex08-3-homework.py
+++ section08/Ex08-3-homework.py
@@ -28,13 +28,19 @@
 *******
  *****
   ***
-   *	
+   *
+
+
+           i    j
+   *       0    j > 3 + i  ' '
+  ***      1    j > 3 + i
+ *****     2    j > 3 + i
+*******    3    j > 3 + i
+ ******    4    j > 9 - i ' '
+  *****    5    j > 9 - i
+   ****    6    j > 9 - i
 
 '''
-
-
-
-
 
 i = 0
 while i < 7:
@@ -42,18 +48,22 @@ while i < 7:
     while j < 7:
         if i < 4:
             if j < 3 - i:
-                print(" ", end="")
+                print(' ', end='')
             elif j > 3 + i:
-                print(" ", end="")
+                print(' ', end='')
             else:
-                print("*", end="")
+                print('*', end='')
         else:
             if j < i - 3:
-                print(" ", end="")
+                print(' ', end='')
             elif j > 9 - i:
-                print(" ", end="")
+                print(' ', end='')
             else:
-                print("*", end="")
-        j += 1
+                print('*', end='')
+        j+=1
+
     print()
-    i += 1
+    i+=1
+
+
+
