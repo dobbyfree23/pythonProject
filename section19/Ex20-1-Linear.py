e273fd80bf59677019823ff32c4db2110b8bea60
ITSC <itsc@example.com>
2024-12-17 20:18:41 +0900

    section19

--- section19/Ex20-1-Linear.py
+++ section19/Ex20-1-Linear.py
@@ -25,6 +25,24 @@ class LinearList():
 
     # 데이터 삽입
     def insert_data(self, position, data):
+        '''
+        linear.insert_data(3, 99)
+        self.linear = [3, 5, 4, 99, 2, 6]
+        position = 3
+        data = 99
+        linearSize = 6
+        5 ~ 4 -1
+        range(5, 3, -1)
+         i = 5
+         linear[5] = 6
+         linear[4] = None
+         i = 4
+         linear[4] = 2
+         linear[3] = None
+
+         linear[3] = 99
+
+        '''
         linear = self.linear
 
         linear.append(None)
@@ -37,6 +55,32 @@ class LinearList():
         linear[position] = data
 
 
+    def delete_data(self, position):
+        '''
+        self.linear = [3, 5, 99, 2, 6]
+        potision = 2
+        linear[2] = None
+        linearSize = 6
+
+        i = 3 ~ 5
+        for i in range(3, 6)
+            i = 5
+            linear[4] = 6
+            linear[5] = None
+
+        del linear[5]
+        '''
+
+        linear = self.linear
+
+        linear[position] = None
+        linearSize = len(linear)
+
+        for i in range(position+1, linearSize):
+            linear[i - 1] = linear[i]
+            linear[i] = None
+
+        del linear[linearSize-1]
 
     def print_list(self):
         linear = self.linear
@@ -51,9 +95,9 @@ linear.add_data(4)
 linear.add_data(2)
 linear.add_data(6)
 
-
 linear.insert_data(3, 99)
 
+linear.delete_data(2)
 
 linear.print_list()
 
