7afa5ad10ea6636c0ff99caaee5b52b921c1cb6b
ITSC <itsc@example.com>
2024-12-26 20:04:02 +0900

    ex23

--- section23/Ex23-2-matplotlib.py
+++ section23/Ex23-2-matplotlib.py
@@ -10,7 +10,7 @@ fig, ax = plt.subplots()
 # 데이터 설정
 fruits = ['apple', 'blueberry', 'cherry', 'orange']
 counts = [40, 100, 30, 55]
-bar_labels = ['red', 'blue', '_red', 'orange']
+bar_labels = ['red', 'blue', '_red', 'orange']  # 각 막대의 레이블 (범례 표시될 레이블- _red 범례 제외)
 bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange'] # 각 막대의 색상
 
 ax.bar(fruits, counts, label=bar_labels, color=bar_colors)
